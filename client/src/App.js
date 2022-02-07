9f9d2d3898f709e85956a12034443cca9fcce8cf
KishParikh13 <kaparikh@mail.smu.edu>
2022-02-06 20:17:42 -0600

    Worked! Readding frontend

--- client/src/App.js
+++ client/src/App.js
@@ -11,26 +11,15 @@ import {
 import Login from './views/Login';
 
 export default function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-      {/* <Router>
+      <Router>
         <Routes>
           <Route exact path="/" element={<Landing/>} />
           <Route path="/login" element={<Login/>} />
         </Routes>
-      </Router> */}
+      </Router>
     </div>
   );
 }
